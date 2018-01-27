dd68a10890f78c0a0cda497881cccb7030ff33eb
tkhm <no-reply@tkhm.dev>
2018-01-27 12:27:30 +0900

    Update sample script and build options
    ベースのスクリプトを今回やろうとしている内容に近づけた https://developer.chrome.com/extensions/samples#search:contextmenus
    またminifyオプションを取り除くことで開発中のデバッグをしやすくした

--- src/scripts/jk.ts
+++ src/scripts/jk.ts
@@ -2,10 +2,14 @@ import Jkcall from "./jkcall";
 
 class Jk {
   constructor() {
-    console.log("jk Works!");
+    console.log("jk Constructor is called.");
     const jkcall = new Jkcall();
     jkcall.echoName();
   }
+
+  public reportProgress(progress: int): void {
+    console.log('progress: ' + progress + ' byte!')
+  }
 }
 
-window.addEventListener("DOMContentLoaded", () => new Jk());
+export default Jk;
