98e9e4c790ad721c1e65557c2ab0d7002ca93cb8
tkhm <no-reply@tkhm.dev>
2018-01-27 12:27:30 +0900

    Update sample script and build options
    ベースのスクリプトを今回やろうとしている内容に近づけた https://developer.chrome.com/extensions/samples#search:contextmenus
    またminifyオプションを取り除くことで開発中のデバッグをしやすくした

--- src/app.js
+++ src/app.js
@@ -1,128 +1,74 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+import Jk from './scripts/jk.ts';
 
-import './scripts/jk.ts';
+// The onClicked callback function.
+function onClickHandler(info, tab) {
+  let jk = new Jk()
+  jk.reportProgress(0)
 
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback called when the URL of the current tab
- *   is found.
- */
-function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
+  if (info.menuItemId == "radio1" || info.menuItemId == "radio2") {
+    console.log("radio item " + info.menuItemId +
+                " was clicked (previous checked state was "  +
+                info.wasChecked + ")");
+  } else if (info.menuItemId == "checkbox1" || info.menuItemId == "checkbox2") {
+    console.log(JSON.stringify(info));
+    console.log("checkbox item " + info.menuItemId +
+                " was clicked, state is now: " + info.checked +
+                " (previous state was " + info.wasChecked + ")");
+  } else {
+    console.log("item " + info.menuItemId + " was clicked");
+    console.log("info: " + JSON.stringify(info));
+    console.log("tab: " + JSON.stringify(tab));
+  }
+};
 
-  chrome.tabs.query(queryInfo, (tabs) => {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
+chrome.contextMenus.onClicked.addListener(onClickHandler);
 
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
+// Set up context menu tree at install time.
+chrome.runtime.onInstalled.addListener(function() {
+  // Create one test item for each context type.
+  var contexts = ["page","selection","link","editable","image","video",
+                  "audio"];
+  for (var i = 0; i < contexts.length; i++) {
+    var context = contexts[i];
+    var title = "Test '" + context + "' menu item";
+    var id = chrome.contextMenus.create({"title": title, "contexts":[context],
+                                         "id": "context" + context});
+    console.log("'" + context + "' item:" + id);
+  }
 
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
+  // Create a parent item and two children.
+  chrome.contextMenus.create({"title": "Test parent item", "id": "parent"});
+  chrome.contextMenus.create(
+      {"title": "Child 1", "parentId": "parent", "id": "child1"});
+  chrome.contextMenus.create(
+      {"title": "Child 2", "parentId": "parent", "id": "child2"});
+  console.log("parent child1 child2");
 
-    callback(url);
-  });
+  // Create some radio items.
+  chrome.contextMenus.create({"title": "Radio 1", "type": "radio",
+                              "id": "radio1"});
+  chrome.contextMenus.create({"title": "Radio 2", "type": "radio",
+                              "id": "radio2"});
+  console.log("radio1 radio2");
 
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, (tabs) => {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-}
+  // Create some checkbox items.
+  chrome.contextMenus.create(
+      {"title": "Checkbox1", "type": "checkbox", "id": "checkbox1"});
+  chrome.contextMenus.create(
+      {"title": "Checkbox2", "type": "checkbox", "id": "checkbox2"});
+  console.log("checkbox1 checkbox2");
 
-/**
- * Change the background color of the current page.
- *
- * @param {string} color The new background color.
- */
-function changeBackgroundColor(color) {
-  var script = 'document.body.style.backgroundColor="' + color + '";';
-  // See https://developer.chrome.com/extensions/tabs#method-executeScript.
-  // chrome.tabs.executeScript allows us to programmatically inject JavaScript
-  // into a page. Since we omit the optional first argument "tabId", the script
-  // is inserted into the active tab of the current window, which serves as the
-  // default.
-  chrome.tabs.executeScript({
-    code: script
-  });
-}
+  // Intentionally create an invalid item, to show off error checking in the
+  // create callback.
+  console.log("About to try creating an invalid item - an error about " +
+      "duplicate item child1 should show up");
 
-/**
- * Gets the saved background color for url.
- *
- * @param {string} url URL whose background color is to be retrieved.
- * @param {function(string)} callback called with the saved background color for
- *     the given url on success, or a falsy value if no color is retrieved.
- */
-function getSavedBackgroundColor(url, callback) {
-  // See https://developer.chrome.com/apps/storage#type-StorageArea. We check
-  // for chrome.runtime.lastError to ensure correctness even when the API call
-  // fails.
-  chrome.storage.sync.get(url, (items) => {
-    callback(chrome.runtime.lastError ? null : items[url]);
-  });
-}
-
-/**
- * Sets the given background color for url.
- *
- * @param {string} url URL for which background color is to be saved.
- * @param {string} color The background color to be saved.
- */
-function saveBackgroundColor(url, color) {
-  var items = {};
-  items[url] = color;
-  // See https://developer.chrome.com/apps/storage#type-StorageArea. We omit the
-  // optional callback since we don't need to perform any action once the
-  // background color is saved.
-  chrome.storage.sync.set(items);
-}
-
-// This extension loads the saved background color for the current tab if one
-// exists. The user can select a new background color from the dropdown for the
-// current page, and it will be saved as part of the extension's isolated
-// storage. The chrome.storage API is used for this purpose. This is different
-// from the window.localStorage API, which is synchronous and stores data bound
-// to a document's origin. Also, using chrome.storage.sync instead of
-// chrome.storage.local allows the extension data to be synced across multiple
-// user devices.
-document.addEventListener('DOMContentLoaded', () => {
-  getCurrentTabUrl((url) => {
-    var dropdown = document.getElementById('dropdown');
-
-    // Load the saved background color for this page and modify the dropdown
-    // value, if needed.
-    getSavedBackgroundColor(url, (savedColor) => {
-      if (savedColor) {
-        changeBackgroundColor(savedColor);
-        dropdown.value = savedColor;
-      }
-    });
-
-    // Ensure the background color is changed and saved when the dropdown
-    // selection changes.
-    dropdown.addEventListener('change', () => {
-      changeBackgroundColor(dropdown.value);
-      saveBackgroundColor(url, dropdown.value);
-    });
-  });
+  //chrome.contextMenus.create({"title": "Oops", "id": "child1"}, function() {
+  //  if (chrome.extension.lastError) {
+  //    console.log("Got expected error: " + chrome.extension.lastError.message);
+  //  }
+  //});
 });
