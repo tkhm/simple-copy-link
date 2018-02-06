e70671092aa43c38e55bbe3b72baae359f8a0aa6
Takayuki Tomita <meltest7@gmail.com>
2018-02-06 21:47:34 +0900

    implement copyStringToClipboard function #1

--- src/scripts/jk.ts
+++ src/scripts/jk.ts
@@ -10,6 +10,18 @@ class Jk {
   public reportProgress(progress: number): void {
     console.log('progress: ' + progress + ' byte!')
   }
+
+  public copyStringToClipboard(string){
+    function handler (event){
+      event.clipboardData.setData('text/plain', string);
+      event.preventDefault();
+      document.removeEventListener('copy', handler, true);
+    }
+  
+    document.addEventListener('copy', handler, true);
+    document.execCommand('copy');
+    console.log(string);
+  }
 }
 
 export default Jk
